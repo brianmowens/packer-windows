acd307173fd63ca24635e0b09bb3a958c20e5b9b
Brian Owens <SESA472364@schneider-electric.com>
2019-06-04 09:49:04 -0400

    Populated build file for 2019 core and reverted build for local testing.

---
+++ appveyor/2019_core.ps1
@@ -0,0 +1,15 @@
+param(
+  [CmdletBinding()]  
+  
+  [Parameter(Mandatory=$false)]
+  [string] $PackerBuilder = "virtualbox-iso",
+
+  [Parameter(Mandatory=$false)]
+  [string] $isoUrl = "$($env:server2019iso)",
+
+  [Parameter(Mandatory=$false)]
+  [string] $packerFile = "..\packer_files\2019_core.json"
+)
+
+Write-Output "Starting build"
+packer build --only=$PackerBuilder --var iso_url=$isoUrl $packerFile
