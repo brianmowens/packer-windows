4d868ed3c94d16761a6fc135e750ac227203cbac
Brian Owens <owens.brianm@gmail.com>
2019-06-05 22:57:52 -0400

    Modified interface query

--- scripts/configure-hyperv-interface.ps1
+++ scripts/configure-hyperv-interface.ps1
@@ -1,8 +1,7 @@
 
 # Configure the VM nic
 if($env:PACKER_BUILDER_TYPE -eq "hyperv-iso"){
-    $Interface = (Get-NetAdapter | Where-Object {$_.Name -like "*Ethernet*"})[0]
-
+    $Interface = Get-NetAdapter | Where-Object {$_.Name -like "*Ethernet*"}
     New-NetIPAddress -InterfaceAlias $Interface.InterfaceAlias -IPAddress 10.1.0.2 -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway 10.1.0.1
     Set-DnsClientServerAddress -InterfaceIndex $Interface.InterfaceIndex -ServerAddresses @("8.8.8.8","8.8.4.4")
 }
