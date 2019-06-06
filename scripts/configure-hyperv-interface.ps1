6eb8cd5c980e6e5ed75a9c35f1a66698215c4551
Brian Owens <owens.brianm@gmail.com>
2019-06-05 21:52:08 -0400

    Modified script to grab dynamic interface.

--- scripts/configure-hyperv-interface.ps1
+++ scripts/configure-hyperv-interface.ps1
@@ -1,8 +1,10 @@
 
 # Configure the VM nic
 if($env:PACKER_BUILDER_TYPE -eq "hyperv-iso"){
-    New-NetIPAddress -InterfaceAlias Ethernet -IPAddress 192.168.0.2 -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway 192.168.0.1
-    Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses @("8.8.8.8","8.8.4.4")
+    $Interface = (Get-NetAdapter | Where-Object {$_.Name -like "*Ethernet*"})[0]
+
+    New-NetIPAddress -InterfaceAlias $Interface.InterfaceAlias -IPAddress 10.1.0.2 -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway 10.1.0.1
+    Set-DnsClientServerAddress -InterfaceIndex $Interface.InterfaceIndex -ServerAddresses @("8.8.8.8","8.8.4.4")
 }
 else{
     Write-Host "Only Hyper-V hosts should run this script."
