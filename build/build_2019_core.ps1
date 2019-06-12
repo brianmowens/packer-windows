4dd96107c883df4a0b3128b2f8288b6db8d86fb0
Brian Owens <SESA472364@schneider-electric.com>
2019-06-12 14:51:12 -0400

    Parameterized hyperv config script. Consolidated build scripts. Switched appveyor build off private cloud and back onto AppVeyor cloud.

--- build/build_2019_core.ps1
+++ build/build_2019_core.ps1
@@ -1,10 +1,15 @@
+param(
+  [CmdletBinding()]  
+  
+  [Parameter(Mandatory=$false)]
+  [string] $PackerBuilder = "hyperv-iso",
 
-packer build `
-  --only=virtualbox-iso `
-  --var iso_url=c:/workbench/software/microsoft/17763.379.190312-0539.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso `
-  ../packer_files/2019_core.json
+  [Parameter(Mandatory=$false)]
+  [string] $isoUrl = "$($env:server2019_iso_output_path)",
 
-if(Test-Path -Path ".\packer_cache"){
-  Write-Output "Clearing packer cache from local disk"
-  Remove-Item .\packer_cache -Recurse -Force
-}
+  [Parameter(Mandatory=$false)]
+  [string] $packerFile = ".\packer_files\2019_core.json"
+)
+
+Write-Output "Starting build"
+packer build --only=$PackerBuilder --var iso_url=$isoUrl $packerFile
--- scripts/configure-hyperv-interface.ps1
+++ scripts/configure-hyperv-interface.ps1
@@ -1,5 +1,37 @@
+# This script is to configure the NIC of a Hyper-V Virtual Machine
+# that is running on a NAT network. 
+
+param(
+    [CmdletBinding()]
+
+    [Parameter(Mandatory=$false)]
+    [string] $IPAddress = "10.1.0.2",
+
+    [Parameter(Mandatory=$false)]
+    [string] $PrefixLength = "24",
+
+    [Parameter(Mandatory=$false)]
+    [string] $DefaultGateway = "10.1.0.1",
+
+    [Parameter(Mandatory=$false)]
+    [array] $DNSServers = @("8.8.8.8","8.8.4.4")
+)
+
+# Check if we're running on the AppVeyor platform
+if($env:APPVEYOR_PROJECT_NAME){
+    Write-Output "Build is running on AppVeyor platform. Configuring NIC."
+}
+# If not in AppVeyor, make sure we're running Hyper-V
+elseif($env:PACKER_BUILDER_TYPE -eq "hyperv-iso"){
+    Write-Output "Packer build type is Hyper-V. Configuring NIC."
+}
+# Exit if we don't have a match
+else {
+    Write-Output "Only AppVeyor and Hyper-V need their NIC configured."
+    break
+}
 
 # Configure the VM nic
 $Interface = Get-NetAdapter | Where-Object {$_.Name -like "*Ethernet*"}
-New-NetIPAddress -InterfaceAlias $Interface.InterfaceAlias -IPAddress 10.1.0.2 -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway 10.1.0.1
-Set-DnsClientServerAddress -InterfaceIndex $Interface.InterfaceIndex -ServerAddresses @("8.8.8.8","8.8.4.4")
+New-NetIPAddress -InterfaceAlias $Interface.InterfaceAlias -IPAddress $IPAddress -AddressFamily IPv4 -PrefixLength $PrefixLength -DefaultGateway $DefaultGateway
+Set-DnsClientServerAddress -InterfaceIndex $Interface.InterfaceIndex -ServerAddresses $DNSServers
